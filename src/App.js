204309e31416b31a56ce42aff4af892efb0de56d
Darkhan Baibulat <darkhanbaibulat@Darkhans-MacBook-Pro.local>
2022-06-15 23:37:10 +0600

    team page update

--- src/App.js
+++ src/App.js
@@ -32,18 +32,18 @@ function App() {
 </div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/talgat.png')} /><br /><br /><h3>Talgat Manglayev</h3><p ><b>Lead Engineer</b></p><p className="font1" >Ph.D. Nazarbayev University Postdoctoral Researcher at University of Helsinki</p> </div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/willie.png')} /><br /><br /><h3>Willie Mclinton</h3><p ><b>Lead Researcher</b></p><p className="font1" >Ph.D. MIT AI and Robotics Google AI Resident Researcher</p></div>
-<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}><img src={require('./avatar/margie.png')} /> <br /><br /><h3>Marguerite Blair</h3><p ><b>Software Engineer</b></p><p className="font1" >Citizens Bank</p></div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/robert.png')} /><br /><br /><h3>Robert Ohr</h3><p ><b>Chairman</b></p><p className="font1" >Lehman Brothers <br />Columbia University</p></div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/aibek.png')} /><br /><br /><h3>Aibek Tleuliyev</h3><p ><b>Lead Security Engineer</b></p><p className="font1" >Head of the Department of Ballistic and Navigation Support of the Mission Control Center</p></div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/dair.png')} /><br /><br /><h3>Dair Alayev</h3><p ><b>Full-stack Developer</b></p><p className="font1" >Astana IT University</p></div>
 <div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/max.png')} /><br /><br /><h3>Max Ward</h3><p ><b>UI/UX</b></p><p className="font1" >Boston University</p></div>
 
-<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/darkhan.svg').default} /><br /><br /><h3>Darkhan Baibulat</h3><p ><b>Front-end Developer</b></p><p className="font1" >Astana IT University</p></div>
-<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/olzhas.svg').default} /><br /><br /><h3>Olzhas Abdykalykov </h3><p ><b>Back-end Developer</b></p><p className="font1" >Astana IT University</p></div>
-<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/kalbek.svg').default} /><br /><br /><h3>Kalbek Saduakassov</h3><p ><b>Back-end Developer</b></p><p className="font1" >Astana IT University</p></div>
-<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/aisha.svg').default} /><br /><br /><h3>Aisha Kozhamseitova</h3><p > <b>Mobile Developer</b></p><p className="font1" >Astana IT University</p></div>
-</div>
+<div className="list_team" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/darkhan.svg').default} /><br /><br /><h3>Darkhan Baibulat</h3><p ><b>Front-end Developer</b></p><p className="font1" >Astana IT University</p></div></div>
 <div className="grid_team_2">
+<div className="list_team2" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/olzhas.svg').default} /><br /><br /><h3>Olzhas Abdykalykov </h3><p ><b>Back-end Developer</b></p><p className="font1" >Astana IT University</p></div>
+<div className="list_team2" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/kalbek.svg').default} /><br /><br /><h3>Kalbek Saduakassov</h3><p ><b>Back-end Developer</b></p><p className="font1" >Astana IT University</p></div>
+<div className="list_team2" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/aisha.svg').default} /><br /><br /><h3>Aisha Kozhamseitova</h3><p > <b>Mobile Developer</b></p><p className="font1" >Astana IT University</p></div>
+
+
 <div className="list_team2" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/hayato.png')} /><br /><br />
 <h3>Hayato Gin Kawai</h3><p ><b>Graphic Design</b></p><p className="font1" >gallerialovelace.io</p></div>
 <div className="list_team2" style = {{backgroundColor: theme.list, color: theme.links}}> <img src={require('./avatar/nathan.png')} /><br /><br />
